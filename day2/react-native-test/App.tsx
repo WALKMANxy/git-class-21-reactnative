0e931a862c9d75d138189bfe1670bfc4f1a52955
WALKMANxy <artorias555@gmail.com>
2024-05-21 01:56:44 +0200

    Adding the project

---
+++ day2/react-native-test/App.tsx
@@ -0,0 +1,63 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View, Button } from "react-native";
+import {
+  ActionSheetProvider,
+  useActionSheet,
+} from "@expo/react-native-action-sheet";
+
+export default function App() {
+  return (
+    <ActionSheetProvider>
+      <View style={styles.container}>
+        <ColorBox />
+        <StatusBar style="auto" />
+      </View>
+    </ActionSheetProvider>
+  );
+}
+
+function ColorBox() {
+  const { showActionSheetWithOptions } = useActionSheet();
+  const [boxColor, setBoxColor] = useState("red");
+
+  const changeColor = () => {
+    const options = ["Red", "Blue", "Green", "Yellow", "Cancel"];
+    const cancelButtonIndex = 4;
+
+    showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      (buttonIndex) => {
+        if (buttonIndex !== undefined && buttonIndex !== cancelButtonIndex) {
+          const selectedColour = options[buttonIndex].toLowerCase();
+          setBoxColor(selectedColour);
+        }
+      }
+    );
+  };
+
+  return (
+    <View style={[styles.redBox, { backgroundColor: boxColor }]}>
+      <Button title="Change Colour" onPress={changeColor} color="#fff" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  redBox: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+});
